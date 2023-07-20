24f243f3b97e38c87fb542fd71913b740782ee1d
Zachary Ringwood <zringwood@gmail.com>
2023-07-20 16:14:38 -0400

    Board now accounts AI making the first move.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import axios from 'axios'
 function App() {
   const [movesPGN, setMovesPGN] = useState("")
   const [positionFEN, setPositionFEN] = useState("")
-  const [puzzleID, setPuzzleID] = useState(12);
+  const [puzzleID, setPuzzleID] = useState(2);
+  const [solver, setSolver] = useState("");
   //Read in the current puzzle from the backend. 
   const apiURL = "http://localhost:8080/"
   useEffect(() => {
     axios.get(`${apiURL}${puzzleID}`).then(response => {
-      console.log(response.data);
       setMovesPGN(response.data.Moves);
       setPositionFEN(response.data.FEN);
+      setSolver(response.data.solver);
+      console.log(response);
     }).catch(response => {
       console.error(response);
     })
@@ -25,9 +27,9 @@ function App() {
   return (
     <>
       <div style={{ width: 500 + "px" }}>
-        <PuzzleBoard positionFEN={positionFEN} movestrPGN={movesPGN} puzzleID={puzzleID}/>
+        <PuzzleBoard positionFEN={positionFEN} movestrPGN={movesPGN} puzzleID={puzzleID} solver={solver}/>
       </div>
-      <button onClick = {() => {setPuzzleID((puzzleID+1))}}>Next Puzzle</button>
+      <button onClick = {() => {setPuzzleID((puzzleID+1)%10)}}>Next Puzzle</button>
     </>
   )
 }
--- src/components/PuzzleBoard/PuzzleBoard.jsx
+++ src/components/PuzzleBoard/PuzzleBoard.jsx
@@ -2,23 +2,22 @@ import { useState, useEffect } from "react"
 import { Chessboard } from "react-chessboard"
 import { Chess } from "chess.js"
 
-let moveIndex = 0
-function PuzzleBoard({ positionFEN, movestrPGN }) {
+
+function PuzzleBoard({ positionFEN, movestrPGN, solver }) {
     const [moveLogic, setMoveLogic] = useState(new Chess(positionFEN))
-    const [position, setPosition] = useState(positionFEN)
+    const [, setPosition] = useState(positionFEN)
+    const [moveIndex, setMoveIndex] = useState(0)
     //We updated the state variables using usEffect so that they actually change on load
     useEffect(() => {
         setPosition(positionFEN);
         setMoveLogic(new Chess(positionFEN));
         //moveIndex must be reset when the puzzle resets.
-        moveIndex = 0;
+        setMoveIndex(0);
     }, [positionFEN])
-    
     const delayInMillis = 500
-    const moveNumberOrGameResult = /(\d\.|[01]-[01])+/
+    const moveNumberOrGameResult = /(\d\.{1,3}|[01]-[01]|\*)+/
     //Convert the move string to a move array and gut the headers. 
-    let movesArray = movestrPGN.split(' ').filter((element) => !element.match(moveNumberOrGameResult))
-   
+    let movesArray = movestrPGN.split(' ').map((element) => element.replace(moveNumberOrGameResult, ''))
     const onDrop = (sourceSquare, targetSquare) => {
         let moveAlgebraic = convertObjectToAlgebraic(sourceSquare, targetSquare)
         
@@ -44,12 +43,19 @@ function PuzzleBoard({ positionFEN, movestrPGN }) {
     }
     const updatePuzzle = (moveAlgebraic) => {
         moveLogic.move(moveAlgebraic)
-        moveIndex += 1
+        setMoveIndex(moveIndex + 1)
         setPosition(moveLogic.fen())
     }
+    //In some cases, the first move of the move array is the opponent's move rather than the players. 
+    //In this case we need to update the board immediately.
+    if(moveLogic.turn() !== solver.charAt(0).toLowerCase() && (movesArray[0].search("...") > -1 && solver === "White")){
+        //First move is for black but the solver is white. 
+        updatePuzzle(movesArray[moveIndex])
+    }
+
     return (
         <>
-            <Chessboard position={position} onPieceDrop={onDrop} />
+            <Chessboard position={moveLogic.fen()} onPieceDrop={onDrop} boardOrientation={solver.toLowerCase()}/>
             {moveIndex >= movesArray.length && <p>You Win!</p>}
         </>
     )
